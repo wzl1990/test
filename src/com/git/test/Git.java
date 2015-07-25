007edde767cb56e5cceec643fe3cf94fbc7c0b1d
weizhenglong <weizhenglong@hyper-telecom.com>
2015-07-25 20:36:57 +0800

    更新代码

--- src/com/git/test/Git.java
+++ src/com/git/test/Git.java
@@ -3,6 +3,6 @@ package com.git.test;
 public class Git {
 	public static void main(String[] args) {
             System.out.println("aaa");
-            System.out.println("bbbbb")
+            System.out.println("bbbbb");
 	}
 }
